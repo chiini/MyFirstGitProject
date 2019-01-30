fb99090e3c80521b380c6a4c484b034454b8bf45
chiini <chiini@hotmail.de>
2019-01-30 17:34:59 +0100

    minor changes and some replacements

---
+++ Assets/MyGame/Scripts/Basics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basics : MonoBehaviour {
+
+    public Text overrideText;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World");
+        overrideText.text = "Dinky, Winky, Dipsy, LaLa, Poo";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    //  Debug.Log("alive");
+    }
+}
---
+++ Assets/MyGame/Scripts/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HelloWorld : MonoBehaviour {
+
+	void Start () {
+      
+	}
+	
+	void Update () {
+		
+	}
+
+    public void DebugLog (){
+        Debug.Log("Hello World");
+    }
+
+}
