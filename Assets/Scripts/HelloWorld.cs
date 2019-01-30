7ea35ff15f5dbfe2e1740e0f84c7a5fb1078c29d
chiini <chiini@hotmail.de>
2019-01-30 17:27:48 +0100

    initial commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;               //Liberary
+using System.Collections.Generic;       //Liberary
+using UnityEngine;                      //Liberary
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       // Console.WriteLine("Hello World");     //nimmt Unity nicht lol
+        Debug.Log("Hello World");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
